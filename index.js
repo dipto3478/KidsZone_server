353245b68310be88275692a536089cba2a625f59
dipto3478 <kitkatboy3478@gmail.com>
2023-05-19 02:33:38 +0600

    delete product system added

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       const result = await kidsZoneCollection.updateOne(query, updateDoc);
       res.send(result);
     });
+    // delete product by id
+    app.delete("/mytoys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await kidsZoneCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
